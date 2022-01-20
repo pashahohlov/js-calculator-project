e5e0ea0bec30b5a7bdce57d34a2e7b9ff16c62fd
Pavel Hohlov <pashahohlov54@gmail.com>
2022-01-20 15:31:12 +0300

    Урок 2(не всё)

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ let adaptive;
 
 alert("Hello world!")
 console.log("Sample text")
+
+title = "Калькулятор вёрстки блоков";
+screens = "Простые, Сложные, Интерактивные";
+screenPrice = 1000;
+rollback = 90;
+fullPrice = 5000;
+adaptive = true;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
