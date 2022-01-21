b450e9fc2f9280a03168e57bbddd891b988b1b72
Pavel Hohlov <pashahohlov54@gmail.com>
2022-01-21 12:21:06 +0300

    Урок 2, задание 1

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ let rollback;
 let fullPrice;
 let adaptive;
 
-alert("Hello world!")
-console.log("Sample text")
+//alert("Hello world!")
+console.log("Kept you waiting, huh?")
 
 title = "Калькулятор вёрстки блоков";
 screens = "Простые, Сложные, Интерактивные";
@@ -18,3 +18,8 @@ adaptive = true;
 console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость вёрстки экранов " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+console.log(screens.toLowerCase());
+console.log("Процент отката посреднику за работу " + fullPrice * (rollback/100));
