01a8e07b5c4e0366fed8e785549147b476a55528
Pavel Hohlov <pashahohlov54@gmail.com>
2022-01-24 13:58:45 +0300

    Урок 3(не закончен)

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let rollback;
 let fullPrice;
 let adaptive;
 
-//alert("Hello world!")
+alert("Приветствие")
 console.log("Kept you waiting, huh?")
 
 title = "Калькулятор вёрстки блоков";
@@ -23,3 +23,21 @@ console.log("Стоимость вёрстки экранов " + screenPrice + 
 console.log("Стоимость разработки сайта " + fullPrice + " рублей");
 console.log(screens.toLowerCase().split());
 console.log("Процент отката посреднику за работу " + fullPrice * (rollback/100));
+
+title = prompt('Как называется ваш проект?');
+console.log(title);
+screens = prompt('Какие типы экранов нужно разработать?', "пример: Простые, Сложные, Интерактивные");
+console.log(screens);
+screenPrice = +prompt('Сколько будет стоить денная работа?', "пример: 12000");
+console.log(screenPrice);
+adaptive = confirm('Нужен ли адаптив на сайте?');
+console.log(adaptive);
+service1 = prompt('Какой дополнительный тип услуги нужен?');
+console.log(service1);
+servicePrice1 = +prompt('Сколько это будет стоить?');
+console.log(servicePrice1);
+service2 = prompt('Какой дополнительный тип услуги нужен?');
+console.log(service2);
+servicePrice2 = +prompt('Сколько это будет стоить?');
+console.log(servicePrice2);
+fullPrice = screenPrice + servicePrice1 + servicePrice2;
