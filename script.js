7e5b05e979bc47970789cabddd7e09aff89d3997
Pavel Hohlov <pashahohlov54@gmail.com>
2022-02-06 19:34:21 +0300

    Урок 4

--- script.js
+++ script.js
@@ -1,59 +1,81 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+'use strict';
 
-alert("Приветствие")
-console.log("Kept you waiting, huh?")
 
-title = "Калькулятор вёрстки блоков";
-screens = "Простые, Сложные, Интерактивные";
-screenPrice = 1000;
-rollback = 15;
-fullPrice = 5000;
-adaptive = true;
+let title = prompt(' “Как называется ваш проект?” ');
+let screens = prompt(' “Какие типы экранов нужно разработать?” ', ' "Простые, Сложные, Интерактивные" ');
+let screenPrice = +prompt(' “Сколько будет стоить данная работа?” ', '12000');
+let adaptive = confirm(' “Нужен ли адаптив на сайте? ”');
+const rollback = 65;
+let service1
+let servicePrice1
+let service2
+let servicePrice2
+let fullPrice
+let servicePrcentPrice
+let allServicePrices
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
-console.log("Стоимость вёрстки экранов " + screenPrice + " рублей");
-console.log("Стоимость разработки сайта " + fullPrice + " рублей");
-console.log(screens.toLowerCase().split());
-console.log("Процент отката посреднику за работу " + fullPrice * (rollback/100));
 
-title = prompt('Как называется ваш проект?');
-console.log(title);
-screens = prompt('Какие типы экранов нужно разработать?', "пример: Простые, Сложные, Интерактивные");
-console.log(screens);
-screenPrice = +prompt('Сколько будет стоить данная работа?', "пример: 12000");
-console.log(screenPrice);
-adaptive = confirm('Нужен ли адаптив на сайте?');
-console.log(adaptive);
-service1 = prompt('Какой дополнительный тип услуги нужен?');
-console.log(service1);
-servicePrice1 = +prompt('Сколько это будет стоить?');
-console.log(servicePrice1);
-service2 = prompt('Какой дополнительный тип услуги нужен?');
-console.log(service2);
-servicePrice2 = +prompt('Сколько это будет стоить?');
-console.log(servicePrice2);
-fullPrice = screenPrice + servicePrice1 + servicePrice2;
-switch (true) {
-    case fullPrice >= 30000:
-        alert("Вам доступна скидка 10%");
-        break
-    case 15000 <= fullPrice < 30000:
-        alert("Вам доступна скидка 5%");
-        break
-    case 0 <= fullPrice < 15000:
-        alert("Скидка не предусмотрена");
-        break
-    default:
-        alert("Что-то пошло не так");
-        break
+const getAllServicePrices = function() {
+    let sum = 0;
+
+    for (let i = 0; i < 2; i++) {
+
+        if( i == 0 ){
+            service1  = prompt(' “Какой дополнительный тип услуги нужен?” ');
+        } else if ( i == 1 ){
+            service2  = prompt(' “Какой дополнительный тип услуги нужен?” ');
+        }
+
+        allPrices += +prompt(' “Сколько это будет стоить?” ');
+        
+    }
+    return sum
 }
-let servicePercentPrice = fullPrice - (fullPrice / 100 * 15);
-console.log(servicePercentPrice);
+
+const showTypeOf = function(variable) {
+    console.log(variable, typeof variable);
+}
+
+const getRollbackMessage = function(price) {
+    if (price >= 30000) {
+        return 'Даем скидку в 10%'
+  } else if (price >= 15000 && price < 30000) {
+    return 'Даем скидку в 5%'
+  } else if (price >= 0 && price < 15000) {
+    return 'Скидка не предусмотрена'
+  } else {
+    return 'Что-то пошло не так'
+  }
+}
+
+function getFullPrice(a,b) {
+    return Number(a)+Number(b)
+} 
+
+const getServicePercentPrices = function(a,b) {
+    return Math.ceil(a-a*(b/100))
+}
+
+const getTitle = function() {
+    return title.trim()[0].toUpperCase() + title.trim().substring(1).toLowerCase()
+}
+
+
+title = getTitle();
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice(screenPrice,allServicePrices);
+servicePrcentPrice = getServicePercentPrices(fullPrice,rollback);
+
+
+showTypeOf(title);
+showTypeOf(fullPrice);
+showTypeOf(adaptive);
+
+console.log(screens.length);
+
+console.log(servicePrcentPrice);
+console.log(allServicePrices);
+console.log(getRollbackMessage(fullPrice));
+
+console.log('“Стоимость верстки экранов '+ screenPrice +' рублей/ долларов/гривен/юани”' );
+console.log('“Стоимость разработки сайта '+ fullPrice +' рублей/ долларов/гривен/юани”' );
